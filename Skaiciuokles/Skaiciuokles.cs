fc2d787b4a2891aa2db188c21589bc34bf984fbe
CalembourIII <paulius.paulaitis@gmail.com>
2023-01-03 13:50:15 +0200

    add project Skaiciuokles

---
+++ Skaiciuokles/Skaiciuokles.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skaiciuokles
+{
+    internal class Skaiciuokles
+    {
+        public static int Suma(int sk1, int sk2)
+        {
+            return sk1 + sk2;
+        }
+
+        public static int Atimtis(int sk1, int sk2)
+        {
+            return sk1 - sk2;
+        }
+        public static int Sandauga(int sk1, int sk2)
+        {
+            return sk1 * sk2;
+        }
+        public static double Dalyba(int sk1, double sk2)
+        {
+            return sk1 / sk2;
+        }
+    }
+}
