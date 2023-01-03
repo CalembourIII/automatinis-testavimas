2ce9d408b47b5aa0bbae240d80e98e38d3e7358e
CalembourIII <paulius.paulaitis@gmail.com>
2023-01-03 13:51:29 +0200

    add project UnitTestai

--- UnitTestai/Properties/AssemblyInfo.cs
+++ UnitTestai/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Test1")]
+[assembly: AssemblyTitle("UnitTestai")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Test1")]
+[assembly: AssemblyProduct("UnitTestai")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("95dbc347-cdae-4dd6-9768-7a90bc071905")]
+[assembly: Guid("5e478036-9525-4700-b6a6-0ae791ba430c")]
 
 // Version information for an assembly consists of the following four values:
 //
