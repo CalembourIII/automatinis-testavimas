fa3106f8e3a738592f30b39e83c70713241649f4
CalembourIII <paulius.paulaitis@gmail.com>
2023-01-04 12:11:09 +0200

    write 2 new SeleniumTests

--- SeleniumTestai/Class1.cs
+++ SeleniumTestai/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V106.DOMSnapshot;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,12 +13,52 @@ namespace SeleniumTestai
     public class Class1
     {
         [Test]
-        public void Testas()
+        public void Testas1()
         {
             IWebDriver driver = new ChromeDriver();
-            driver.Url = "https://www.google.com";
-            driver.Manage().
-            
+
+            string expectedOutput = "Labas";
+
+            driver.Url = "https://demoqa.com/text-box";
+            IWebElement inputElement = driver.FindElement(By.XPath("//*[@ID=\"userName\"]"));
+            inputElement.SendKeys("Labas");
+
+            string actualOutput = inputElement.Text;
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+
+            driver.Quit();
+
+
+
+            //driver.FindElement(By.CssSelector("#userName"));
+            //driver.FindElement(By.Id("userName"));
+
+
+
+
+            //driver.FindElement(By.XPath(""));
+            //driver.FindElement(By.CssSelector(""));
+            //driver.FindElement(By.Id(""));
         }
+
+        [Test]
+        public void Testas2()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            string expectedResult = "Text Box";
+
+            driver.Url = "https://demoqa.com/text-box";
+            IWebElement inputElement = driver.FindElement(By.XPath("//*[@class='main-header']"));
+
+            string actualResult = inputElement.Text;
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+            driver.Quit();
+
+        }
+
     }
 }
