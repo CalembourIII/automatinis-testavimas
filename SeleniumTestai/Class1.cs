44e3e8b6e6f576b89c98a22f90fcc1629dbe2fd6
CalembourIII <paulius.paulaitis@gmail.com>
2023-01-04 10:34:02 +0200

    add new project SeleniumTestai

---
+++ SeleniumTestai/Class1.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTestai
+{
+    public class Class1
+    {
+        [Test]
+        public void Testas()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.google.com";
+            driver.Manage().
+            
+        }
+    }
+}
