09cfca150d7351694955855b2131e31f5b2ecb0f
CalembourIII <paulius.paulaitis@gmail.com>
2023-01-05 16:02:41 +0200

    Add new static test "BasicCheckboxDemo" (+ methods)

---
+++ FrameworkStatic/Pages/SeleniumEasy/BasicCheckboxDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkStatic.Pages.SeleniumEasy
+{
+    public class BasicCheckboxDemo
+    {
+        private static string url = "https://demo.seleniumeasy.com/basic-checkbox-demo.html";
+        private static string singleCheckboxLocator = "//*[@id=\"isAgeSelected\"]";
+        private static string multipleCheckboxLocator = "";
+        private static string successMessageLocator = "//*[@id=\"txtAge\"]";
+
+        public static void Open()
+        {
+            Driver.OpenPage(url);
+        }
+
+        public static void ClickSingleCheckbox()
+        {
+            Common.ClickElement(singleCheckboxLocator);
+        }
+
+        public static string GetSuccessMessage()
+        {
+        return Common.GetElementText(successMessageLocator);
+        }
+    }
+}
---
+++ TestsStatic/SeleniumEasy/BasicCheckboxDemoTests.cs
@@ -0,0 +1,37 @@
+﻿using FrameworkStatic;
+using FrameworkStatic.Pages.SeleniumEasy;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsStatic.SeleniumEasy
+{
+    internal class BasicCheckboxDemoTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.Initialize();
+            BasicCheckboxDemo.Open();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            //Driver.CloseDriver();
+        }
+
+        [Test]
+        public static void Click_Single_Checkbox()
+        {
+            string expectedResult = "Success - Check box is checked";
+            BasicCheckboxDemo.ClickSingleCheckbox();
+            string actualResult = BasicCheckboxDemo.GetSuccessMessage();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
