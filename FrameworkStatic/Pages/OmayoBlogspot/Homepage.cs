8e629b348930e001174fd08854fe1412a1fa3264
CalembourIII <paulius.paulaitis@gmail.com>
2023-01-05 14:03:52 +0200

    Add new class "BasicFirstFormDemo" test methods

--- FrameworkStatic/Pages/OmayoBlogspot/Homepage.cs
+++ FrameworkStatic/Pages/OmayoBlogspot/Homepage.cs
@@ -12,8 +12,8 @@ namespace FrameworkStatic.Pages.OmayoBlogspot
     public class Homepage
     {
 
-        private static string locatorHtmlFormUserName = "//form[@name='form1']/input[@type='text']"; // lokatorius apsirasau klases virsuj, po to pernaudoju
-        private static string locatorTextBoxWithpreloadedText = "//*[@id=\"textbox1\"]";
+        private static string htmlFormUserNameLocator = "//form[@name='form1']/input[@type='text']"; // lokatorius apsirasau klases virsuj, po to pernaudoju
+        private static string textBoxWithpreloadedTextLocator = "//*[@id=\"textbox1\"]";
 
         public static void Open()
         {
@@ -38,7 +38,7 @@ namespace FrameworkStatic.Pages.OmayoBlogspot
             Common.ClearInputElement(locator);
         }
 
-        public static void EnterTextBoxWithPreloadedText(string value)
+        public static void EnterTextToBoxWithPreloadedText(string value)
         {
             string locator = "//*[@id=\"textbox1\"]";
             Common.SendKeysToElement(locator, value);
---
+++ FrameworkStatic/Pages/SeleniumEasy/BasicFirstFormDemo.cs
@@ -0,0 +1,64 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkStatic.Pages.SeleniumEasy
+{
+    internal class BasicFirstFormDemo
+    {
+        private static string url = "https://demo.seleniumeasy.com/basic-first-form-demo.html";
+        private static string InputMessageLocator = "//*[@id=\"user-message\"]";
+        private static string buttonShowMessageLocator = "//*[@id='get-input']/button";
+        private static string spanMessageLocator = "//*[@id=\"display\"]";
+        private static string divFullMessageLocator = "//div[@id=\"user-message\"]";
+        private static string enterSum1Locator = "//*[@id=\"sum1\"]";
+        private static string enterSum2Locator = "//*[@id=\"sum2\"]";
+        private static string getTotalButtonLocator = "//*[@id=\"gettotal\"]/button";
+        private static string displayTotalValueLocator = "//*[@id=\"displayvalue\"]";
+
+        public static void Open()
+        {
+            Driver.OpenPage(url);
+        }
+
+        public static void EnterMessage(string name)
+        {
+            Common.SendKeysToElement(InputMessageLocator, name);
+        }
+
+        public static void ClickShowMessageButton()
+        {
+            Common.ClickElement(buttonShowMessageLocator);
+        }
+
+        public static string GetValueOfMessage()
+        {
+            return Common.GetElementText(spanMessageLocator);
+        }
+
+        public static string GetValueOfFullMessage()
+        {
+            return Common.GetElementText(divFullMessageLocator);
+        }
+
+        public static void EnterValueToSum1(string value)
+        {
+            Common.SendKeysToElement(enterSum1Locator, value);
+        }
+        public static void EnterValueToSum2(string value)
+        {
+            Common.SendKeysToElement(enterSum2Locator, value);
+        }
+        public static void ClickGetTotalButton()
+        {
+            Common.ClickElement(getTotalButtonLocator);
+        }
+        public static string GetValueOfDisplayTotalValue()
+        {
+            return Common.GetElementText(displayTotalValueLocator);
+        }
+    }
+}
