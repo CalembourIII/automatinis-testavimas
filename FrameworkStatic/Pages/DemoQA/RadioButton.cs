0a05bcd8c23a56cf18db25154e1eda7b15247354
CalembourIII <paulius.paulaitis@gmail.com>
2023-01-06 15:41:08 +0200

    Add new project "RadioButtonTests" (+methods)

---
+++ FrameworkStatic/Pages/DemoQA/RadioButton.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkStatic.Pages.DemoQA
+{
+    public class RadioButton
+    {
+        private static string url = "https://demoqa.com/radio-button";
+        private static string yesButtonLocator = "//*[@for='yesRadio']";
+        private static string impressiveButtonLocator = "//*[@for='impressiveRadio']";
+        private static string noButtonLocator = "//*[@id='noRadio']";
+        private static string spanTextSuccessLocator = "//*[contains(@class,'text-success')]";
+
+        public static void Open()
+        {
+            Driver.OpenPage(url);
+        }
+        public static void YesRadioButtonClick()
+        {
+            Common.ClickElement(yesButtonLocator);
+        }
+
+        public static void ImpressiveRadioButtonClick()
+        {
+            Common.ClickElement(impressiveButtonLocator);
+        }
+
+        public static void NoRadioButtonClick()
+        {
+            Common.ClickElement(noButtonLocator);
+        }
+
+        public static string GetMessageSelectedConfirmation()
+        {
+            return Common.GetElementText(spanTextSuccessLocator);
+        }
+
+        public static bool CheckIfButtonNoIsEnabled()
+        {
+           return Common.GetElementEnabledStatus(noButtonLocator);
+        }
+    }
+}
---
+++ TestsStatic/DemoQA/RadioButtonTests.cs
@@ -0,0 +1,55 @@
+﻿using FrameworkStatic.Pages.DemoQA;
+using FrameworkStatic;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsStatic.DemoQA
+{
+    internal class RadioButtonTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.Initialize();
+            RadioButton.Open();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            Driver.CloseDriver();
+        }
+
+        [Test]
+        public static void Select_Yes_Button()
+        {
+            string expectedValue = "Yes";
+            RadioButton.YesRadioButtonClick();
+            string actualValue = RadioButton.GetMessageSelectedConfirmation();
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [Test]
+        public static void Select_Impressive_Button()
+        {
+            string expectedValue = "Impressive";
+            RadioButton.ImpressiveRadioButtonClick();
+            string actualValue = RadioButton.GetMessageSelectedConfirmation();
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [Test]
+        public static void Check_If_Button_No_Is_Disabled()
+        {
+            bool radioButtonNoEnabled = RadioButton.CheckIfButtonNoIsEnabled();
+
+            Assert.AreEqual(false, radioButtonNoEnabled);
+        }
+    }
+}
