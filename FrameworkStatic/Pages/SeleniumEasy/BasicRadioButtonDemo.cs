42f7cb16f1bd6b9b3b039d3e06e03053456e0f28
CalembourIII <paulius.paulaitis@gmail.com>
2023-01-06 16:06:42 +0200

    Add new project "BasicRadioButtonDemoTests" (+methods)
    Written 1 Test as an example

---
+++ FrameworkStatic/Pages/SeleniumEasy/BasicRadioButtonDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkStatic.Pages.SeleniumEasy
+{
+    public class BasicRadioButtonDemo
+    {
+        private static string url = "https://demo.seleniumeasy.com/basic-radiobutton-demo.html";
+        private static string maleRadioButtonLocator = "(//input[@value='Male'])[1]";
+        private static string femaleRadioButtonLocator = "(//input[@value='Female'])[1]";
+        private static string getCheckedValueButtonLocator = "//*[@id='buttoncheck']";
+        private static string getCheckedValueMessageLocator = "//*[@class='radiobutton']";
+
+        public static void Open()
+        {
+            Driver.OpenPage(url);
+        }
+
+        public static void ClickMaleRadioButton()
+        {
+            Common.ClickElement(maleRadioButtonLocator);
+        }
+
+        public static void ClickFemaleRadioButton()
+        {
+            Common.ClickElement(femaleRadioButtonLocator);
+        }
+
+        public static void ClickGetCheckedValueButton()
+        {
+            Common.ClickElement(getCheckedValueButtonLocator);
+        }
+        public static string getCheckedValueMessage()
+        {
+            return Common.GetElementText(getCheckedValueMessageLocator);
+        }
+    }
+}
---
+++ TestsStatic/SeleniumEasy/BasicRadioButtonDemoTests.cs
@@ -0,0 +1,39 @@
+﻿using FrameworkStatic.Pages.SeleniumEasy;
+using FrameworkStatic;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FrameworkStatic.Pages.DemoQA;
+
+namespace TestsStatic.SeleniumEasy
+{
+    internal class BasicRadioButtonDemoTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.Initialize();
+            BasicRadioButtonDemo.Open();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            //Driver.CloseDriver();
+        }
+
+        [Test]
+        public static void Select_Male_Button()
+        {
+            string expectedValue = "Radio button 'Male' is checked";
+            BasicRadioButtonDemo.ClickMaleRadioButton();
+            BasicRadioButtonDemo.ClickGetCheckedValueButton();
+            string actualValue = BasicRadioButtonDemo.getCheckedValueMessage();
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
