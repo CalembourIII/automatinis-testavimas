d5f37fb682508ece5e26a764da2df32930847acc
CalembourIII <paulius.paulaitis@gmail.com>
2023-01-06 14:04:36 +0200

    Add new project "DynamicPropertiesTests" (+methods)

---
+++ FrameworkStatic/Pages/DemoQA/DynamicProperties.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkStatic.Pages.DemoQA
+{
+    public class DynamicProperties
+    {
+        private static string url = "https://demoqa.com/dynamic-properties";
+        private static string ButtonToBeClickableLocator = "//*[@id='enableAfter']";
+        private static string ButtonToChangeColorLocator = "//*[@id='colorChange']";
+        private static string buttonToBePresentLocator = "//*[@id='visibleAfter']";
+
+        public static void Open()
+        {
+            Driver.OpenPage(url);
+        }
+
+        public static void WaitForButtonToBeClickable()
+        {
+            Common.WaitForElementToBeClickable(ButtonToBeClickableLocator);
+        }
+
+        public static void WaitForButtonToChangeColor()
+        {
+            Common.WaitForElementAttributeToContainValue(ButtonToChangeColorLocator, "class", "text-danger");
+        }
+
+        public static void WaitForButtonToBeVisible()
+        {
+            Common.WaitForElementToBeVisible(buttonToBePresentLocator);
+        }
+    }
+}
---
+++ TestsStatic/DemoQA/DynamicPropertiesTests.cs
@@ -0,0 +1,46 @@
+﻿using FrameworkStatic.Pages.OmayoBlogspot;
+using FrameworkStatic;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FrameworkStatic.Pages.DemoQA;
+
+namespace TestsStatic.DemoQA
+{
+    internal class DynamicPropertiesTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.Initialize();
+            DynamicProperties.Open();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            Driver.CloseDriver();
+        }
+
+        [Test]
+        public void Wait_For_Button_To_Be_Clickable()
+        {
+            DynamicProperties.WaitForButtonToBeClickable();
+        }
+
+        [Test]
+        public void Wait_For_Button_To_Change_Color()
+        {
+            DynamicProperties.WaitForButtonToChangeColor();
+        }
+
+        [Test]
+        public void Wait_For_Button_To_Be_Visible()
+        {
+            DynamicProperties.WaitForButtonToBeVisible();
+        }
+    }
+}
