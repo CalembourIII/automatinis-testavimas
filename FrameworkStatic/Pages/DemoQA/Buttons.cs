ecf4621a29fe56e860ebc2b93e75015a21ed65df
CalembourIII <paulius.paulaitis@gmail.com>
2023-01-09 14:24:15 +0200

    New test project (Buttons) in DemoQA (+methods)

---
+++ FrameworkStatic/Pages/DemoQA/Buttons.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkStatic.Pages.DemoQA
+{
+    public class Buttons
+    {
+        private static string url = "https://demoqa.com/buttons";
+        private static string doubleClickMeButtonLocator = "//*[@id='doubleClickBtn']";
+        private static string rightClickMeButtonLocator = "//*[@id='rightClickBtn']";
+        private static string clickMeButtonLocator = "//*[text()='Click Me']";
+        private static string doubleClickMeButtonMessageLocator = "//*[@id='doubleClickMessage']";
+        private static string rightClickMeButtonMessageLocator = "//*[@id='rightClickMessage']";
+        private static string clickMeButtonMessageLocator = "//*[@id='dynamicClickMessage']";
+
+        public static void Open()
+        {
+            Driver.OpenPage(url);
+        }
+
+        public static void DoubleClickButton()
+        {
+            Common.DoubleClickElement(doubleClickMeButtonLocator);
+        }
+
+        public static void RightClickButton()
+        {
+            Common.RightClickElement(rightClickMeButtonLocator);
+        }
+
+        public static void LeftClickButton()
+        {
+            Common.LeftClickElement(clickMeButtonLocator);
+        }
+
+        public static string GetDoubleClickMessage()
+        {
+            return Common.GetElementText(doubleClickMeButtonMessageLocator);
+        }
+        public static string GetRightClickMessage()
+        {
+            return Common.GetElementText(rightClickMeButtonMessageLocator);
+        }
+
+        public static string GetLeftClickMessage()
+        {
+            return Common.GetElementText(clickMeButtonMessageLocator);
+        }
+    }
+}
---
+++ TestsStatic/DemoQA/ButtonsTests.cs
@@ -0,0 +1,61 @@
+﻿using FrameworkStatic;
+using FrameworkStatic.Pages.DemoQA;
+using FrameworkStatic.Pages.SeleniumEasy;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsStatic.DemoQA
+{
+    internal class ButtonsTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.Initialize();
+            Buttons.Open();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            Driver.CloseDriver();
+        }
+
+        [Test]
+        public void Click_Left_Button()
+        {
+            string expectedMessage = "You have done a dynamic click";
+
+            Buttons.LeftClickButton();
+            string actualMessage = Buttons.GetLeftClickMessage();
+
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+
+        [Test]
+        public void Click_Right_Button()
+        {
+            string expectedMessage = "You have done a right click";
+
+            Buttons.RightClickButton();
+            string actualMessage = Buttons.GetRightClickMessage();
+
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+
+        [Test]
+        public void Double_Click_Button()
+        {
+            string expectedMessage = "You have done a double click";
+
+            Buttons.DoubleClickButton();
+            string actualMessage = Buttons.GetDoubleClickMessage();
+
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+    }
+}
