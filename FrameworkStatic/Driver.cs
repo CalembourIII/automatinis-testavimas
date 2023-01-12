2c41f4d8407b7f0628b79e1c013e84007834d717
CalembourIII <paulius.paulaitis@gmail.com>
2023-01-12 16:32:50 +0200

    Add method to start tests with oher profile

--- FrameworkStatic/Driver.cs
+++ FrameworkStatic/Driver.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
+using Selenium.WebDriver.UndetectedChromeDriver;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,8 +14,8 @@ namespace FrameworkStatic
 {
     public class Driver
     {
-
         private static ThreadLocal<IWebDriver> driver = new ThreadLocal<IWebDriver>();
+        //private static IWebDriver driver;
 
         public static void Initialize()
         {
@@ -24,6 +25,7 @@ namespace FrameworkStatic
             //options.AddArgument("headless");
             //options.AddArgument("window-size=1920,1080");
 
+            //driver = UndetectedChromeDriver.Instance("selenium"); //sita naudojam kai bnorim pasileisti per testa narsykle su sukurtu profiliu. Bet reikia paleisti ir 18 eulutej driveri tada
             driver.Value = new ChromeDriver(options);
         }
 
@@ -31,6 +33,7 @@ namespace FrameworkStatic
         {
             return driver.Value;
         }
+
         public static void OpenPage(string url)
         {
             driver.Value.Url = url;
